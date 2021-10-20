fe5b58c5bc8751004733923571af3072fc3720e9
aaditya prabu <aadityaprabu@gmail.com>
2021-10-20 16:26:49 +0530

    can make docs without codes

--- Automator/Src/automator.py
+++ Automator/Src/automator.py
@@ -20,28 +20,40 @@ hSize = 0
 doc.add_heading(heading, hSize)
 doc.paragraphs[-1].alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
 
-codeExt = input('Enter the extention for your codes:')
-codeFiles = glob.glob(codeFilePath+"*.{0}".format(codeExt))
-codeFont = 'Courier New'
+e = input('do you have codes in your assignment ?(y/n)')
+
+if e == 'Y' or e == 'y':
+    codeExt = input('Enter the extention for your codes:')
+    codeFiles = glob.glob(codeFilePath+"*.{0}".format(codeExt))
+    codeFont = 'Courier New'
 
 imgExt = input('Enter the extention for your images:')
 imageFiles = (glob.glob(imageFilePath+"*.{0}".format(imgExt)))
 
 
-for fileIdx in range(len(codeFiles)):
-    subHeading = input('Enter a subheading:')
-    sub = doc.add_heading(subHeading, 2)
+if e == 'Y' or e == 'y':
+    for fileIdx in range(len(codeFiles)):
+        subHeading = input('Enter a subheading:')
+        sub = doc.add_heading(subHeading, 2)
 
-    cpath = codeFilePath+"{0}.{1}".format(fileIdx+1, codeExt)
-    code = open(cpath)
-    para = doc.add_paragraph()
-    for line in code:
-        temp = para.add_run(line)
-        temp.font.name = codeFont
-        temp.bold = True
-    code.close()
-    ipath = imageFilePath+"{0}.{1}".format(fileIdx+1, imgExt)
-    doc.add_picture(ipath, width=Inches(docWidth.inches-2))
-    doc.paragraphs[-1].alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+        cpath = codeFilePath+"{0}.{1}".format(fileIdx+1, codeExt)
+        code = open(cpath)
+        para = doc.add_paragraph()
+        for line in code:
+            temp = para.add_run(line)
+            temp.font.name = codeFont
+            temp.bold = True
+        code.close()
+        ipath = imageFilePath+"{0}.{1}".format(fileIdx+1, imgExt)
+        doc.add_picture(ipath, width=Inches(docWidth.inches-2))
+        doc.paragraphs[-1].alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+else:
+    for fileIdx in range(len(imageFiles)):
+        subHeading = input('Enter a subheading:')
+        sub = doc.add_heading(subHeading, 2)  
+        ipath = imageFilePath+"{0}.{1}".format(fileIdx+1, imgExt)
+        doc.add_picture(ipath, width=Inches(docWidth.inches-2))
+        doc.paragraphs[-1].alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.CENTER
+
 
 doc.save(docpath+'{0}.docx'.format(docName))
